7f316cd361e41e52070533508732c37b6c39e915
Thompson Naidu <thompsonnaiducm1343@gmail.com>
2025-06-28 18:58:18 -0400

    adding graphql resolver and service with apollo graph ql and nest js

---
+++ apps/backend/src/tag/entities/tag.entity.ts
@@ -0,0 +1,20 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Post } from 'src/post/entities/post.entity';
+
+@ObjectType()
+export class Tag {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field(() => [Post])
+  posts: [Post];
+
+  @Field()
+  createAt: Date;
+
+  @Field()
+  updatedAt: Date;
+}
